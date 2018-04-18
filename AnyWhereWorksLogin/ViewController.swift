2c32a826f9fb6483f73830cb8d05726f10c17a08
User <user@full157-air.full.creative>
2018-04-18 11:04:28 +0530

    log message to the app launch in the app delegate created a button to the main.storyboard prints when button pressed

--- AnyWhereWorksLogin/ViewController.swift
+++ AnyWhereWorksLogin/ViewController.swift
@@ -13,8 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("this is a git tutorial")
     }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        print("this is a button")
+        print("this is a git tutorial")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
